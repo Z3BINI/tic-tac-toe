7f2c6bf3e56428862dd53fa53e4c766784a10427
Eusébio Alves <eusebio.a95@hotmail.com>
2023-09-02 11:34:12 +0100

    Started gameBoard controller module
    The gameboard is getting initialized and an array of objects is created
    with the elments and necessary info for posterior manipulation. A click
    event was created and receives the clicked target event and relevant
    square object.

--- script/script.js
+++ script/script.js
@@ -1,1 +1,26 @@
-console.log('lalal');
+const gameBoard = (function(){
+
+    //Cache DOM
+    const gameSquareElements = document.querySelectorAll('.game-square');
+
+    //Add all elements & necessary element info in gameBoardSquares Array
+    const gameBoardSquares = Array.from(gameSquareElements)
+                             .map(gameSquareElement => {
+                                return { 
+                                    element: gameSquareElement,
+                                    position: gameSquareElement.dataset.pos,
+                                    currentMark: gameSquareElement.textContent
+                                };
+                             });
+
+    
+    //Add event listeners
+    gameBoardSquares.forEach(square => square.element.addEventListener('click', (event) => squareController(event, square)));
+
+    //Gameboard functions
+    const squareController = (event, square) => {
+        console.log(event.target);
+        console.log(square);
+    }
+
+})();
