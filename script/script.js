cf6ecfdb339f6b640b3b2dc48a96a553d91977d2
Eusébio Alves <eusebio.a95@hotmail.com>
2023-09-02 13:26:21 +0100

    Added IIFE init and Player factory
    Now dialog is shown at start with a player form. Players will be created
    on submit and sent to the gameBoard module.

--- script/script.js
+++ script/script.js
@@ -1,4 +1,38 @@
-const gameBoard = (function(){
+const init = (function(){
+
+    //Cache form DOM
+    const formDialog = document.querySelector('dialog');
+    const form = document.querySelector('form');
+
+    //Show the dialog by default
+    formDialog.showModal();
+
+    //Listen for start game
+    form.addEventListener('submit', () => validateInfo());
+
+    const validateInfo = function() {
+        let player1Name = document.querySelector('input#player1').value;
+        let player1Mark = 'X';
+        let player2Name = document.querySelector('input#player2').value;
+        let player2Mark = 'O';
+
+        const player1 = Player(player1Name, player1Mark);
+        const player2 = Player(player2Name, player2Mark);
+        
+        gameBoard(player1, player2);
+    }
+
+   
+
+})();
+
+const Player = function(playerName, playerMark) {
+    return {playerName, playerMark}
+}
+
+const gameBoard = function(player1, player2){
+
+    console.log({player1, player2});
 
     //Cache DOM
     const gameSquareElements = document.querySelectorAll('.game-square');
@@ -23,4 +57,4 @@ const gameBoard = (function(){
         console.log(square);
     }
 
-})();
+};
